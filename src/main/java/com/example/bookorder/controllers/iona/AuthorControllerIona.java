eefea9696a6b6592188b9079d672beba6fcd609e
Muthia Rahma <muthiaarahmaa@gmail.com>
2020-11-25 17:00:29 +0700

    menambahkan custom on insert publisher controller

--- src/main/java/com/example/bookorder/controllers/iona/AuthorControllerIona.java
+++ src/main/java/com/example/bookorder/controllers/iona/AuthorControllerIona.java
@@ -1,14 +1,47 @@
 package com.example.bookorder.controllers.iona;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bookorder.models.Author;
 import com.example.bookorder.models.dtos.AuthorDto;
+import com.io.iona.core.enums.ActionFlow;
+import com.io.iona.implementations.pagination.DefaultPagingParameter;
+import com.io.iona.springboot.actionflows.custom.CustomAfterReadAll;
+import com.io.iona.springboot.actionflows.custom.CustomBeforeInsert;
 import com.io.iona.springboot.controllers.HibernateCRUDController;
+import com.io.iona.springboot.sources.HibernateDataSource;
+import com.io.iona.springboot.sources.HibernateDataUtility;
 
 @RestController
 @RequestMapping("/api/author-iona")
-public class AuthorControllerIona extends HibernateCRUDController<Author, AuthorDto>{
+public class AuthorControllerIona extends HibernateCRUDController<Author, AuthorDto> implements CustomAfterReadAll<Author, AuthorDto>
+	/*implements CustomBeforeInsert<Author, AuthorDto>*/{
 
+	ModelMapper mapper = new ModelMapper();
+	
+	/*@Override
+	public void beforeInsert(HibernateDataUtility hibernateDataUtility, HibernateDataSource<Author, AuthorDto> dataSource) throws Exception {
+		Author author = dataSource.getDataModel();
+		author.setAuthorId(1);
+		
+	}*/
+	
+	@Override
+	public List<AuthorDto> afterReadAll(HibernateDataUtility hibernateDataUtility, HibernateDataSource<Author, AuthorDto> dataSource,
+			DefaultPagingParameter defaultPagingParameter) throws Exception {
+		List<Author> author = dataSource.getResult(ActionFlow.ON_READ_ALL_ITEMS, List.class);
+		List<AuthorDto> authorDto = new ArrayList<AuthorDto>();
+		
+		for (Author a : author) {
+			AuthorDto dto = mapper.map(a, AuthorDto.class);
+			authorDto.add(dto);
+		}
+		
+		return authorDto;
+	}
 }
--- src/main/java/com/example/bookorder/controllers/iona/PublisherControllerIona.java
+++ src/main/java/com/example/bookorder/controllers/iona/PublisherControllerIona.java
@@ -1,14 +1,47 @@
 package com.example.bookorder.controllers.iona;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.bookorder.models.Paper;
 import com.example.bookorder.models.Publisher;
 import com.example.bookorder.models.dtos.PublisherDto;
+import com.example.bookorder.repositories.PaperRepository;
+import com.example.bookorder.repositories.PublisherRepository;
+import com.io.iona.springboot.actionflows.custom.CustomOnInsert;
 import com.io.iona.springboot.controllers.HibernateCRUDController;
+import com.io.iona.springboot.sources.HibernateDataSource;
+import com.io.iona.springboot.sources.HibernateDataUtility;
 
 @RestController
 @RequestMapping("/api/publisher-iona")
-public class PublisherControllerIona extends HibernateCRUDController<Publisher, PublisherDto> {
+public class PublisherControllerIona extends HibernateCRUDController<Publisher, PublisherDto> implements CustomOnInsert<Publisher, PublisherDto> {
 
+	@Autowired
+	PublisherRepository publisherRepository;
+	@Autowired
+	PaperRepository paperRepository;
+	
+	@Override
+	public Publisher onInsert(HibernateDataUtility hibernateDataUtility, HibernateDataSource<Publisher, PublisherDto> dataSource) throws Exception {
+		Publisher publisher = dataSource.getDataModel();
+		List<Paper> list = paperRepository.findAll();
+		Paper paper = new Paper();
+	    
+		for (Paper p : list) {
+		    if (p.getPaperId() != publisher.getPaper().getPaperId()) {
+		        paper.setPaperId(publisher.getPaper().getPaperId());
+		        paper.setPaperPrice(new BigDecimal(1000));
+		        paper.setQualityName("Paper Baru");
+		    }
+		}
+		    
+		paperRepository.save(paper);
+		publisherRepository.save(publisher);
+		return dataSource.getDataModel();
+	}
 }
